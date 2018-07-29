02a459d89cb1725983b5cd0bbe0c62bcd6f1c274
Leonard Law <leon.wlaw@gmail.com>
2018-07-28 23:56:14 -0400

    Upgrade vue version, add configuration to fix github pages deployment

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@
 set -e
 
 # build
-npm run docs:build
+NODE_ENV=production yarn run build
 
 # navigate into the build output directory
-cd docs/.vuepress/dist
+cd dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:linkleonard/arowana-marketing.git master:gh-pages
 
 cd -
