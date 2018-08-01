56130afdd0a0c9239a6de3d2bde0140569e4b5c2
Leonard Law <leon.wlaw@gmail.com>
2018-08-01 18:55:50 -0400

    Add controls and styling to gallery

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -1,13 +1,14 @@
 <template>
 <section>
   <h2>Gallery</h2>
-  <div class="container">
-    <img :src="images[selectedImage]" />
-  </div>
-  <div class="controls">
-    <button @click="previousImage">Previous</button>
-    <button>Pause</button>
-    <button @click="nextImage">Next</button>
+  <div class="container" @mouseover="disableImageRotation" @mouseout="enableImageRotation">
+    <div class="carousel">
+      <img :src="images[selectedImage]" />
+    </div>
+    <div class="controls">
+      <button @click="previousImage">&lt;</button>
+      <button @click="nextImage">&gt;</button>
+    </div>
   </div>
 </section>
 </template>
@@ -33,19 +34,32 @@ const Gallery = {
     },
     changeSelectedIndex(index) {
       this.selectedImage = (this.selectedImage + index + this.images.length) % this.images.length
+    },
+    enableImageRotation() {
+      this.startTimer();
+    },
+    disableImageRotation() {
+      this.cleanupTimer();
+    },
+    startTimer() {
+      this.timer = setTimeout(this.updateImageAndSetTimer, 3000);
+    },
+    cleanupTimer() {
+      if (this.timer !== undefined) {
+        clearTimeout(this.timer);
+      }
+    },
+    updateImageAndSetTimer() {
+      this.cleanupTimer();
+      this.changeSelectedIndex(1);
+      this.startTimer();
     }
   },
   mounted() {
-    const updateImageAndSetTimer = () => {
-      this.changeSelectedIndex(1);
-      this.timer = setTimeout(updateImageAndSetTimer, 3000);
-    }
-    updateImageAndSetTimer();
+    this.updateImageAndSetTimer();
   },
   beforeDestroy() {
-    if (this.timer !== undefined) {
-      clearTimeout(this.timer);
-    }
+    this.cleanupTimer();
   },
 };
 
@@ -58,20 +72,54 @@ h2 {
     margin-bottom: 50px;
 }
 
-.container {
-    position: relative;
+.carousel {
     display: flex;
     align-items: center;
     justify-content: center;
+}
+
+img {
+    flex: 1 1 auto;
+}
+
+.container {
+    position: relative;
     max-height: 60vh;
     overflow: hidden;
 }
 
 .controls {
-
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    margin-left: auto;
+    margin-right: auto;
+    margin-bottom: 10px;
 }
 
-img {
-    flex: 1 1 auto;
+.controls :first-child {
+    border-radius: 20px 0 0 20px;
+}
+
+.controls :last-child {
+    border-radius: 0 20px 20px 0;
+}
+
+button {
+    font-size: 24px;
+    font-weight: 600;
+    color: white;
+    background: rgba(0, 0, 0, 0.03);
+    padding: 10px 20px;
+    transition: background 100ms ease-in-out;
+}
+
+button:hover {
+    background: rgba(0, 0, 0, 0.3);
+}
+
+button:focus {
+    outline: none;
 }
 </style>
