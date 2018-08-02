010abd58d9454803fc3ca33a6b3ec20c1ed6e391
Leonard Law <leon.wlaw@gmail.com>
2018-08-02 18:27:28 -0400

    Add picture to join us section

--- src/components/JoinUsSection.vue
+++ src/components/JoinUsSection.vue
@@ -1,10 +1,12 @@
 <template>
 <section :id="id">
   <h2>Join Us</h2>
-  <p>Like water sports?</p>
-  <p>Want to be part of a team of like-minded people?</p>
-  <p>Join us at practice!</p>
-  <a :href="formLink">Sign up</a>
+  <div>
+    <p>Like water sports?</p>
+    <p>Want to be part of a team of like-minded people?</p>
+    <p>Join us at practice!</p>
+    <a :href="formLink">Sign up</a>
+  </div>
 </section>
 </template>
 
@@ -24,11 +26,13 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h2 {
-    margin-bottom: 50px;
-}
-
 a {
     @extend %main-button;
+}
+
+div {
+    padding-top: 50px;
+    background: $darken-background-layer, top / cover url("https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/21761727_10209559180960412_4046836709571286068_n.jpg?_nc_cat=0&oh=4c04362424115025e56813ba20e9ac76&oe=5B905D10");
+    text-shadow: $darken-text-shadow;
 }
 </style>
